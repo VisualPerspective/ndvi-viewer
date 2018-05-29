eea711718ecd3506caa3c8acb0e08d11f08a6de2
Kevin James <kevinwjames@gmail.com>
2018-05-29 14:17:48 -0400

    start integrating d3 for charting

--- src/js/components/Footer.tsx
+++ src/js/components/Footer.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react'
-import { strings } from '../constants'
 import { inject, observer } from 'mobx-react'
+import { strings } from '../constants'
 import RootStore from '../models/RootStore'
 
-const Footer = ({ rootStore } : { rootStore?: RootStore }) => (
-  <footer>
-    <input type="range"
-      min={0}
-      max={rootStore.timePeriods - 1}
-      value={rootStore.timePeriod}
-      onChange={e => {
-        rootStore.timePeriod = Number(e.target.value)
-      }}/>
-  </footer>
-)
+class Footer extends React.Component<{
+  rootStore?: RootStore,
+}, any> {
+  render () {
+    const { rootStore } = this.props
+
+    return (
+      <footer>
+        <svg className="slider">
+          <line x1={0} y1={0} x2={0} y2={10} />
+        </svg>
+        <input type="range"
+          min={0}
+          max={rootStore.timePeriods - 1}
+          value={rootStore.timePeriod}
+          onChange={e => {
+            rootStore.timePeriod = Number(e.target.value)
+          }} />
+      </footer>
+    )
+  }
+}
 
 export default inject('rootStore')(observer(Footer))
