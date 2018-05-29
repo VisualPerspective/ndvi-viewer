7b2fa479fc70cb703822cf57e820ba2db0b9dd19
Kevin James <kevinwjames@gmail.com>
2018-05-29 14:44:59 -0400

    start drawing line indicating selected time period

--- src/js/components/TimeSeries.tsx
+++ src/js/components/TimeSeries.tsx
@@ -3,21 +3,33 @@ import { inject, observer } from 'mobx-react'
 import { strings } from '../constants'
 import RootStore from '../models/RootStore'
 
-const TimeSeries: React.SFC<{
+class TimeSeries extends React.Component<{
   width: number,
   height: number,
   rootStore?: RootStore
-}> = ({ width, height, rootStore }) => (
-  (width && height) ? (
-    <svg className="time-series">
-      <line
-        x1={rootStore.timePeriod / rootStore.timePeriods * width}
-        y1={0}
-        x2={rootStore.timePeriod / rootStore.timePeriods * width}
-        y2={height}
-      />
-    </svg>
-  ) : null
-)
+}> {
+  render () {
+    const { width, height, rootStore } = this.props
+    const xAxisHeight = 20
+    const yAxisWidth = 20
+
+    const chartWidth = width - yAxisWidth
+    const chartHeight = height - xAxisHeight
+
+    const brushPosition = rootStore.timePeriod /
+      (rootStore.timePeriods - 1) * chartWidth
+
+    return (width && height) ? (
+      <svg className="time-series">
+        <line
+          x1={brushPosition}
+          y1={height - xAxisHeight}
+          x2={brushPosition}
+          y2={0}
+        />
+      </svg>
+    ) : null
+  }
+}
 
 export default inject('rootStore')(observer(TimeSeries))
