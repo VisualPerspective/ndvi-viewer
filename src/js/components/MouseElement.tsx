22422a20d18333cda78bb44ab89bdf401eb2a08e
Kevin James <kevinwjames@gmail.com>
2018-05-30 15:08:59 -0400

    create MouseElement to help with mouse and drag support

---
+++ src/js/components/MouseElement.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import { inject, observer } from 'mobx-react'
+import { action, observable, reaction, IReactionDisposer } from 'mobx'
+import Point from '../models/Point'
+
+class MouseElement extends React.Component<{
+  render: ({}: {
+    dragging: boolean,
+    mousePosition?: Point,
+    startDragging: () => void
+  }) => React.ReactElement<any>
+}, any> {
+  @observable mousePosition: Point = new Point(0, 0)
+  @observable dragging: boolean
+
+  mouseUpListener = () => {
+    this.dragging = false
+  }
+
+  mouseMoveListener = (e: MouseEvent) => {
+    this.mousePosition.set(e.screenX, e.screenY)
+  }
+
+  startDragging () {
+    this.dragging = true
+  }
+
+  componentDidMount () {
+    document.addEventListener('mouseup', this.mouseUpListener)
+    document.addEventListener('mousemove', this.mouseMoveListener)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('mouseup', this.mouseUpListener)
+    document.removeEventListener('mousemove', this.mouseMoveListener)
+  }
+
+  render () {
+    return this.props.render({
+      dragging: this.dragging,
+      mousePosition: this.mousePosition,
+      startDragging: this.startDragging.bind(this)
+    })
+  }
+}
+
+export default observer(MouseElement)
