154b2866bf0e9e29c5cf240597be340b1a4d9a25
Kevin James <kevinwjames@gmail.com>
2018-06-01 14:13:41 -0400

    allow clicking anywhere on time series

--- src/js/components/timeSeries/Chart.tsx
+++ src/js/components/timeSeries/Chart.tsx
@@ -58,15 +58,15 @@ class Chart extends React.Component<{
       + margin.left
 
     return (width && height) ? (
-      <svg className='time-series' ref={ref => this.chart = ref}>
+      <svg className='time-series' ref={ref => this.chart = ref}
+        onMouseDown={() => this.props.startDragging()}>
         <YAxis width={width} height={height} margin={margin} />
         <XAxis width={width} height={height} margin={margin} rootStore={rootStore} />
-        <g transform={translate(brushPosition, margin.top)} className='brush'
-          onMouseDown={() => this.props.startDragging()}>
+        <g transform={translate(brushPosition, margin.top)} className='brush'>
           <line x1={0.5} y1={height - margin.top} x2={0.5} y2={0} />
           <g transform={translate(0, height - margin.top - 3)}>
             <polygon points='-3,0 -13,-10 -3,-20' />
-            <polygon points='4,0 14,-10 4,-20' />
+            <polygon points='4.5,0 14.5,-10 4.5,-20' />
           </g>
         </g>
       </svg>
