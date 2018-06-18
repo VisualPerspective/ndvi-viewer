559cb1a57c4ed7b558854f2f6a7f79485a9c2572
Kevin James <kevinwjames@gmail.com>
2018-06-18 15:38:31 -0400

    Remove select boxes from header

--- src/js/components/Header.tsx
+++ src/js/components/Header.tsx
@@ -4,24 +4,6 @@ import { strings } from '@app/constants'
 const Header = () => (
   <header>
     <h1>{strings.HEADING}</h1>
-    <p className='form-line'>
-      <span>Show </span>
-      <select>
-        <option>timeline</option>
-        <option>grid</option>
-      </select>
-      <span> of </span>
-      <select>
-        <option>NDVI</option>
-        <option>NDVI trend</option>
-        <option>NDVI anomoly</option>
-      </select>
-      <span> by </span>
-      <select>
-        <option>month</option>
-        <option>year</option>
-      </select>
-    </p>
   </header>
 )
 
