8e2ccce8245890aeacfaad4d913334a4849ef89d
Kevin James <kevinwjames@gmail.com>
2018-07-20 12:54:34 -0400

    remove mobx dev tools

--- src/js/components/Container.tsx
+++ src/js/components/Container.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import Helmet from 'react-helmet'
 import { inject, observer } from 'mobx-react'
-import DevTools from 'mobx-react-devtools'
 import Header from '@app/components/Header'
 import Footer from '@app/components/Footer'
 import SingleViewContainer from '@app/components/SingleViewContainer'
@@ -10,7 +9,6 @@ import RootStore from '@app/models/RootStore'
 
 const Container: React.SFC<{ rootStore?: RootStore }> = (props) => (
   <>
-    <DevTools />
     <Helmet title='Iceland NDVI'>
       <link href='https://fonts.googleapis.com/css?family=Heebo:300,400,500' rel='stylesheet' />
     </Helmet>
