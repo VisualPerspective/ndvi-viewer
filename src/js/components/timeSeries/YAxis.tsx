945137c5182760aad4a5a84bb5bc1a9b261a9dfe
Kevin James <kevinwjames@gmail.com>
2018-07-31 17:28:48 -0400

    Work on making scale clearer

--- src/js/components/timeSeries/YAxis.tsx
+++ src/js/components/timeSeries/YAxis.tsx
@@ -35,27 +35,30 @@ const YAxis = ({ xScale, yScale, colorScale }: {
           }
         </linearGradient>
       </defs>
-      <g transform={translate(xScale.range()[0] - 46, 0)}>
+      <g transform={translate(xScale.range()[0] - 40, 0)}>
+        <rect fill='url(#grad1)' fill-opacity='0.3'
+          x='0' y={yScale(1)}
+          width='32' height={yScale.range()[0] - yScale.range()[1]} />
         <g transform={translate(0, yScale(1))} className='tick y-tick'>
-          <line x1='0' y1='0.5' x2='32' y2='0.5' />
+          <line x1='0' y1='0.5' x2='33' y2='0.5' />
         </g>
         <image href='/img/ndvi-high.svg'
           width='30' height='30'
-          x='0' y={yScale(0.8) - 15} />
+          x='1' y={yScale(0.8) - 15} />
         <g transform={translate(0, yScale(0.6))} className='tick y-tick'>
-          <line x1='0' y1='0.5' x2='32' y2='0.5' />
+          <line x1='0' y1='0.5' x2='32' y2='0.5' className='white' />
         </g>
         <image href='/img/ndvi-medium.svg'
           width='30' height='30'
-          x='0' y={yScale(0.4) - 15} />
+          x='1' y={yScale(0.4) - 15} />
         <g transform={translate(0, yScale(0.2))} className='tick y-tick'>
-          <line x1='0' y1='0.5' x2='32' y2='0.5' />
+          <line x1='0' y1='0.5' x2='32' y2='0.5' className='white' />
         </g>
         <image href='/img/ndvi-low.svg'
           width='30' height='30'
-          x='0' y={yScale(0) - 15} />
+          x='1' y={yScale(0) - 15} />
         <g transform={translate(0, yScale(-0.2))} className='tick y-tick'>
-          <line x1='0' y1='0.5' x2='32' y2='0.5' />
+          <line x1='0' y1='0.5' x2='33' y2='0.5' />
         </g>
       </g>
       <rect x={xScale.range()[0] - leftScaleWidth}
@@ -72,10 +75,15 @@ const YAxis = ({ xScale, yScale, colorScale }: {
               (i / (constants.DATA_Y_TICKS.length - 1)) *
               (yScale.range()[0] - yScale.range()[1]),
             )}>
-            <text dy='6' x='-43' y='0'>{formatter(tick)}</text>
-            <line x1={-leftScaleWidth} y1='0.5'
+            <line x1={leftScaleWidth} y1='0.5'
               x2={(xScale.range()[1] - xScale.range()[0]) + leftScaleWidth}
               y2='0.5' />
+            <line x1='0' y1='0.5' className={
+              i > 0 && i < constants.DATA_Y_TICKS.length - 1 ? 'white' : ''
+            }
+              x2={leftScaleWidth}
+              y2='0.5' />
+            <text dy='6' x='-38' y='0'>{formatter(tick)}</text>
           </g>
         ))
       }
